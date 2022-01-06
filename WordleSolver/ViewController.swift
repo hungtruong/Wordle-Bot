3b9fbf078ca2f00df59c1194e4a93a034b153c24
Hung Truong <hung@hung-truong.com>
2022-01-06 12:04:28 -0800

    Initial Commit

---
+++ WordleSolver/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WordleSolver
+//
+//  Created by Hung Truong on 1/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
